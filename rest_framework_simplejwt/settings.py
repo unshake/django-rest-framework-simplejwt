ac91aa7e3659a52b9e46b49592bae1925689ca2e
Edgar Martinez <edgarmartinezme@gmail.com>
2020-07-14 15:09:11 -0600

    Add missing comma

--- rest_framework_simplejwt/settings.py
+++ rest_framework_simplejwt/settings.py
@@ -26,7 +26,7 @@ DEFAULTS = {
     'AUTH_HEADER_TYPES': ('Bearer',),
     'USER_ID_FIELD': 'id',
     'USER_ID_CLAIM': 'user_id',
-    'CUSTOM_USER': get_user_model()
+    'CUSTOM_USER': get_user_model(),
 
     'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
     'TOKEN_TYPE_CLAIM': 'token_type',
